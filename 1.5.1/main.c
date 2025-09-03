57a11aa1035e03b2099337f5c1a8723fd6b3980d
m-m-legend <nintendo_64m@outlook.com>
2025-09-03 20:00:37 -0300

    Trabalho 1.5.1

---
+++ 1.5.1/main.c
@@ -0,0 +1,95 @@
+#include <SDL2/SDL.h>
+
+int AUX_WaitEventTimeoutCount(SDL_Event* evt, int ms){
+    Uint32 antes = SDL_GetTicks();
+    int isevt = SDL_WaitEventTimeout(evt, ms);
+    if(isevt){
+        ms -= (SDL_GetTicks() - antes);
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char** args){
+    
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("1.5.1",
+                        SDL_WINDOWPOS_UNDEFINED,
+                        SDL_WINDOWPOS_UNDEFINED,
+                        200, 100, SDL_WINDOW_SHOWN
+    );
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+
+    /* EXECUCAO */
+
+    SDL_Rect r = {20,20,20,10}; //retangulo pelo teclado
+    SDL_Rect t = {20, 40, 20, 10}; //retangulo pelo tempo
+    SDL_Rect l = {20,60,20,10}; //retangulo pelo mouse
+
+    SDL_Event evt;
+
+
+    while(!SDL_QuitRequested()){
+        SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0x00);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0xFF, 0x00);
+        SDL_RenderFillRect(ren, &r); 
+        SDL_RenderFillRect(ren, &t);
+        SDL_RenderFillRect(ren, &l);
+
+        if(t.x > 200 - t.w) t.x = 0;
+        if (t.x < 0) t.x = 0;
+        if (t.y < 0) t.y = 0;
+        if (t.x > 200 - t.w) t.x = 0;
+        if (t.y > 100 - t.h) t.y = 0;
+
+        t.x += 10; t.y += 10;
+        
+        int espera = 200;
+        int isevt = AUX_WaitEventTimeoutCount(&evt, espera);
+        if(isevt){
+
+            if(evt.type == SDL_KEYDOWN){
+                        switch(evt.key.keysym.sym){
+                            case SDLK_UP:
+                                r.y -= 5;
+                                break;
+                            case SDLK_DOWN:
+                                r.y += 5;
+                                break;
+                            case SDLK_LEFT:
+                                r.x -= 5;
+                                break;
+                            case SDLK_RIGHT:
+                                r.x += 5;
+                                break;
+                        }
+                        if (r.x < 0) r.x = 0;
+                        if (r.y < 0) r.y = 0;
+                        if (r.x > 200 - r.w) r.x = 200 - r.w;
+                        if (r.y > 100 - r.h) r.y = 100 - r.h;
+                    } else if(evt.type == SDL_MOUSEMOTION){
+                        l.x = evt.motion.x;
+                        l.y = evt.motion.y;
+                    }
+
+                    } else{
+                        espera = 200;
+                    }
+        
+        SDL_RenderPresent(ren);
+
+    }
+    
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+
+
+    
+    return 0;
+}
